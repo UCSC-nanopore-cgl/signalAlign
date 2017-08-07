6dc0ba9d4b78a1418696dad80b99df9ddd9e0e39
Andrew Bailey <bailey.andrew4@gmail.com>
2017-08-07 10:13:05 -0700

    update setup.py and marginalign

--- setup.py
+++ setup.py
@@ -8,11 +8,12 @@ setup(name="signalAlign",
       author="Art Rand",
       author_email="arand@soe.ucsc.edu",
       url="https://github.com/UCSC-nanopore-cgl/signalAlign",
-      packages=['src'],
-      install_requires=["numpy>=1.9.2",
-                        "h5py>=2.2.1",
-                        "pysam>=0.8.2.1",
-                        "pandas>=0.18.1",
-                        "sonLib>=1.1.0",
-                        "PyYAML>=3.12"]
+      package_dir={"": "src"},
+      packages=find_packages("src")
+      # install_requires=["numpy>=1.9.2",
+      #                   "h5py>=2.2.1",
+      #                   "pysam>=0.8.2.1",
+      #                   "pandas>=0.18.1",
+      #                   "sonLib>=1.1.0",
+      #                   "PyYAML>=3.12"]
       )
