e3d82780cb589bb113f720d58836525720fe5de9
Andrew Bailey <bailey.andrew4@gmail.com>
2018-06-15 10:10:13 -0700

    Fixed some bugs in trinModels.py

--- src/signalalign/train/trainModels.py
+++ src/signalalign/train/trainModels.py
@@ -25,61 +25,6 @@ from signalalign.utils.bwaWrapper import buildBwaIndex
 from py3helpers.utils import merge_lists
 
 
-# def parse_args():
-#     parser = ArgumentParser(description=__doc__)
-#     # required arguments
-#     parser.add_argument('--file_directory', '-d', action='append', default=None,
-#                         dest='files_dir', required=True, type=str,
-#                         help="directories with fast5 files to train on. example: ../reads/")
-#     parser.add_argument('--ref', '-r', action='store', default=None, dest='ref', required=True, type=str,
-#                         help="location of refrerence sequence in FASTA, example: ../ref.fasta")
-#     parser.add_argument('--output_location', '-o', action='store', dest='out', default=None,
-#                         required=True, type=str,
-#                         help="directory to put the trained model, and use for working directory. example: ./scratch/")
-#     # optional arguments
-#     parser.add_argument("--2d", action='store_true', dest="twoD", default=False, help="flag, reads are 2D chemistry.")
-#     parser.add_argument("--bwt", action='store', dest="bwt", default=None,
-#                         help="path to BWT files. example: ../ref.fasta")
-#     parser.add_argument('--stateMachineType', '-smt', action='store', dest='stateMachineType', type=str,
-#                         default="threeState", required=False,
-#                         help="StateMachine options: threeState, threeStateHdp")
-#     parser.add_argument("--file_of_files", "-fofn", action="append", required=False, default=None, dest="fofn",
-#                         type=str, help="text file with absolute paths of files to use")
-#     parser.add_argument('--iterations', '-i', action='store', dest='iter', default=10,
-#                         required=False, type=int, help='number of iterations to perform')
-#     parser.add_argument('--train_amount', '-a', action='store', dest='amount', default=15000,
-#                         required=False, type=int,
-#                         help="limit the total length of sequence to use in training (batch size).")
-#     parser.add_argument('--in_template_hmm', '-T', action='store', dest='in_T_Hmm',
-#                         required=True, type=str, help="template model to bootstrap from, find a starting model in the "
-#                                                       "models directory")
-#     parser.add_argument('--in_complement_hmm', '-C', action='store', dest='in_C_Hmm',
-#                         required=True, type=str,
-#                         help="complement model to bootstrap from, find a starting model in the "
-#                              "models directory")
-#     parser.add_argument('--templateHDP', '-tH', action='store', dest='templateHDP', default=None,
-#                         help="path to template HDP model to use")
-#     parser.add_argument('--complementHDP', '-cH', action='store', dest='complementHDP', default=None,
-#                         help="path to complement HDP model to use")
-#     parser.add_argument('--jobs', '-j', action='store', dest='nb_jobs', required=False, default=4,
-#                         type=int, help="number of jobs to run concurrently")
-#     parser.add_argument('--test', action='store_true', default=False, dest='test', help="Used for CI testing")
-#     parser.add_argument('--ambiguity_positions', '-p', action='store', required=False, default=None,
-#                         dest='substitution_file', help="Substitution positions")
-#     parser.add_argument("--motif", action="store", dest="motif_key", default=None)
-#     parser.add_argument('--ambig_char', '-X', action='append', required=False, default=None, type=str, dest='labels',
-#                         help="Character to substitute at positions, default is 'X'.")
-#     parser.add_argument('--diagonalExpansion', '-e', action='store', dest='diag_expansion', type=int,
-#                         required=False, default=None,
-#                         help="number of diagonals to expand around each anchor default: 50")
-#     parser.add_argument('--constraintTrim', '-m', action='store', dest='constraint_trim', type=int,
-#                         required=False, default=None, help='amount to remove from an anchor constraint')
-#     parser.add_argument('--debug', action='store_true', dest="DEBUG", default=False)
-#
-#     args = parser.parse_args()
-#     return args
-
-
 def generateConfig(config_path):
     if os.path.exists(config_path):
         raise RuntimeError
@@ -153,7 +98,7 @@ def trainHMM(samples, working_folder, config, transitions=True, emissions=False)
     if transitions is False:
         assert emissions is True, "Must select either transitions or emissions to train"
 
-    # make directory to put the files we're using
+    # set all variables from config file
     alignment_file = config["alignment_file"]
     training_amount = config["training_bases"]
     workers = config["job_count"]
@@ -169,6 +114,7 @@ def trainHMM(samples, working_folder, config, transitions=True, emissions=False)
     test = config["test"]
     path_to_bin = config["path_to_bin"]
 
+    # get working folder path and check if there is an alignment file
     working_folder_path = working_folder.path
     if alignment_file:
         alignment_file = os.path.abspath(alignment_file)
@@ -221,8 +167,7 @@ def trainHMM(samples, working_folder, config, transitions=True, emissions=False)
         template_hdp = None
         complement_hdp = None
 
-    i = 0
-    # start iterating
+    # collect signal alignment arguments
     alignment_args = create_signalAlignment_args(
         destination=working_folder_path,
         stateMachineType=state_machine_type,
@@ -237,11 +182,10 @@ def trainHMM(samples, working_folder, config, transitions=True, emissions=False)
         get_expectations=True,
         path_to_bin=path_to_bin)
 
+    i = 0
+    # start iterating
     while i < iterations:
-        # first cull a set of files to get expectations on
-        # training_files = cull_training_files(samples=samples,
-        #                                      training_amount=config["training_bases"],
-        #                                      twoD=config["twoD"])
+        # align all the samples
         samples = multithread_signal_alignment_samples(samples, alignment_args, workers, trim=training_amount)
         all_sample_files = merge_lists([sample.analysis_files for sample in samples])
         assert len(all_sample_files) > 0, "Something failed in multithread signal alignment. We got no sample files"
@@ -321,7 +265,7 @@ def main():
         working_folder = FolderHandler()
         working_folder.open_folder(os.path.join(config['output_dir'], "temp_trainModels"))
 
-        samples = [SignalAlignSample(working_dir=working_folder, **s) for s in config["samples"]]
+        samples = [SignalAlignSample(working_folder=working_folder, **s) for s in config["samples"]]
         # Train HMM transitions
         trainHMM(samples, working_folder, config, transitions=True)
 
