01e3fe26c64ff1b415e4a968696927756a36d6a0
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2018-11-02 16:23:45 -0700

    add pathos dependency

--- setup.py
+++ setup.py
@@ -78,5 +78,6 @@ setup(name="signalAlign",
                         "PyYAML>=3.12",
                         "Cython>=0.26",
                         "scikit-learn==0.19.0",
-                        "matplotlib==2.0.2"]
+                        "matplotlib==2.0.2",
+                        "pathos==0.2.1"]
       )
