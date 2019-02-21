e750fb0ea0ef0c66ff873e16f10e6e31b86eee81
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2019-02-21 15:03:12 -0800

    update scikit-learn requirement

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(name="signalAlign",
                         "sonLib>=1.1.0",
                         "PyYAML>=3.12",
                         "Cython>=0.26",
-                        "scikit-learn==0.19.0",
+                        "scikit-learn>=0.19.0",
                         "matplotlib==2.0.2",
                         "pathos==0.2.1",
                         "py3helpers==0.1.2"]
