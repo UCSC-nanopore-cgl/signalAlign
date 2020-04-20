2891a62257cbd7eec6ff06af651ea045b165cc59
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-04-20 15:39:17 -0700

    update remove_sa_analysis to have basecall and all analysis options

--- src/signalalign/remove_sa_analyses.py
+++ src/signalalign/remove_sa_analyses.py
@@ -19,7 +19,16 @@ def parse_args():
     # required arguments
     parser.add_argument('--directory', '-d', required=True, action='store',
                         dest='dir', type=str, default=None,
-                        help="Path to json config file")
+                        help="Path to directory of fast5 files")
+    parser.add_argument('--analysis', required=False, action='store_true',
+                        dest='analysis', default=False,
+                        help="Remove all analysis files")
+    parser.add_argument('--basecall', required=False, action='store_true',
+                        dest='basecall', default=False,
+                        help="Remove all basecall files")
+    parser.add_argument('--signalalign', required=False, action='store_true',
+                        dest='signalalign', default=False,
+                        help="Remove all signalalign files")
 
     args = parser.parse_args()
     return args
@@ -37,6 +46,31 @@ def remove_sa_analyses(fast5):
     return counter
 
 
+def remove_basecall_analyses(fast5):
+    """Remove basecall analyses from a fast5 file"""
+    assert os.path.exists(fast5), "Fast5 path does not exist".format(fast5)
+    fh = Fast5(fast5, read='r+')
+    counter = 0
+    for analyses in [x for x in list(fh["Analyses"].keys()) if "Basecall" in x]:
+        fh.delete(os.path.join("Analyses", analyses))
+        counter += 1
+    fh.close()
+    return counter
+
+
+def remove_analyses(fast5):
+    """Remove analyses from a fast5 file"""
+    assert os.path.exists(fast5), "Fast5 path does not exist".format(fast5)
+    fh = Fast5(fast5, read='r+')
+    counter = 0
+    for analyses in [x for x in list(fh["Analyses"].keys())]:
+        fh.delete(os.path.join("Analyses", analyses))
+        counter += 1
+    fh.delete("Analyses")
+    fh.close()
+    return counter
+
+
 def main():
     args = parse_args()
     total = 0
@@ -44,7 +78,13 @@ def main():
     errors = 0
     for f5_file in list_dir(args.dir, ext="fast5"):
         try:
-            total += remove_sa_analyses(f5_file)
+            if args.analysis:
+                total += remove_analyses(f5_file)
+            else:
+                if args.signalalign or not args.basecall:
+                    total += remove_sa_analyses(f5_file)
+                elif args.basecall:
+                    total += remove_basecall_analyses(f5_file)
             files += 1
         except KeyError as e:
             errors += 1
