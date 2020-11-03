ee3499a69d49867dfdb29221cfbb723ad9c72030
Andrew D Bailey IV <bailey.andrew4@gmail.com>
2020-11-03 10:51:18 -0800

    removed output from filter reads tests

--- src/signalalign/tests/test_filter_reads.py
+++ src/signalalign/tests/test_filter_reads.py
@@ -15,6 +15,7 @@ import unittest
 import tempfile
 import shutil
 import pysam
+from py3helpers.utils import captured_output
 from signalalign.filter_reads import *
 
 
@@ -34,12 +35,13 @@ class FilterReadsTest(unittest.TestCase):
         cls.bam = os.path.join(cls.HOME, "tests/minion_test_reads/oneD.bam")
 
     def test_write_readdb(self):
-        with tempfile.TemporaryDirectory() as temp_dir:
-            tmp_file = os.path.join(temp_dir, "test.readdb")
-            write_this = [[name, path] for name, path in parse_readdb(self.readdb, [self.test_dir])]
-            write_readdb(write_this, tmp_file)
-            read_this = [[name, path] for name, path in parse_readdb(tmp_file, [self.test_dir])]
-            self.assertSequenceEqual(read_this, write_this)
+        with captured_output() as (_, _):
+            with tempfile.TemporaryDirectory() as temp_dir:
+                tmp_file = os.path.join(temp_dir, "test.readdb")
+                write_this = [[name, path] for name, path in parse_readdb(self.readdb, [self.test_dir])]
+                write_readdb(write_this, tmp_file)
+                read_this = [[name, path] for name, path in parse_readdb(tmp_file, [self.test_dir])]
+                self.assertSequenceEqual(read_this, write_this)
 
     def test_parse_readdb(self):
         for name, path in parse_readdb(self.readdb, [self.test_dir]):
@@ -47,57 +49,63 @@ class FilterReadsTest(unittest.TestCase):
             self.assertTrue(os.path.exists(os.path.join(self.test_dir, path)))
 
     def test_filter_reads(self):
-        first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)}
-        self.assertEqual(len(first_dir), 3)
-        first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=12)}
-        self.assertEqual(len(first_dir), 0)
-        first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [os.path.split(self.test_dir)[0]],
-                                                   quality_threshold=7, recursive=True)}
-        self.assertEqual(len(first_dir), 3)
+        with captured_output() as (_, _):
+            first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)}
+            self.assertEqual(len(first_dir), 3)
+            first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=12)}
+            self.assertEqual(len(first_dir), 0)
+            first_dir = {x: y for x, y in filter_reads(self.bam, self.readdb, [os.path.split(self.test_dir)[0]],
+                                                       quality_threshold=7, recursive=True)}
+            self.assertEqual(len(first_dir), 3)
 
     def test_filter_reads_to_string_wrapper(self):
-        a = filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)
-        for x, y in a:
-            self.assertTrue(type(x) is str)
-            self.assertTrue(type(y) is pysam.AlignedSegment)
+        with captured_output() as (_, _):
+            a = filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)
+            for x, y in a:
+                self.assertTrue(type(x) is str)
+                self.assertTrue(type(y) is pysam.AlignedSegment)
 
-        a = filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)
-        b = filter_reads_to_string_wrapper(a)
-        for x, y in b:
-            self.assertTrue(type(x) is str)
-            self.assertTrue(type(y) is str)
+            a = filter_reads(self.bam, self.readdb, [self.test_dir], quality_threshold=7)
+            b = filter_reads_to_string_wrapper(a)
+            for x, y in b:
+                self.assertTrue(type(x) is str)
+                self.assertTrue(type(y) is str)
 
     def test_multiprocess_move_and_filter_reads(self):
-        with tempfile.TemporaryDirectory() as tempdir:
-            multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=False,
-                                               quality_threshold=False, worker_count=1, debug=True)
-            self.assertEqual(3, len(os.listdir(os.path.join(tempdir, "test"))))
-            shutil.rmtree(self.test_dir)
-            shutil.copytree(self.dna_dir, self.test_dir)
+        with captured_output() as (_, _):
 
-        with tempfile.TemporaryDirectory() as tempdir:
-            multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=10,
-                                               quality_threshold=False, worker_count=1, debug=True)
-            self.assertEqual(1, len(os.listdir(os.path.join(tempdir, "test"))))
-            shutil.rmtree(self.test_dir)
-            shutil.copytree(self.dna_dir, self.test_dir)
-        with tempfile.TemporaryDirectory() as tempdir:
-            multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=False,
-                                               quality_threshold=False, worker_count=1, debug=False)
-            self.assertEqual(3, len(os.listdir(os.path.join(tempdir, "test"))))
-            shutil.rmtree(self.test_dir)
-            shutil.copytree(self.dna_dir, self.test_dir)
+            with tempfile.TemporaryDirectory() as tempdir:
+                multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=False,
+                                                   quality_threshold=False, worker_count=1, debug=True)
+                self.assertEqual(3, len(os.listdir(os.path.join(tempdir, "test"))))
+                shutil.rmtree(self.test_dir)
+                shutil.copytree(self.dna_dir, self.test_dir)
+
+            with tempfile.TemporaryDirectory() as tempdir:
+                multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=10,
+                                                   quality_threshold=False, worker_count=1, debug=True)
+                self.assertEqual(1, len(os.listdir(os.path.join(tempdir, "test"))))
+                shutil.rmtree(self.test_dir)
+                shutil.copytree(self.dna_dir, self.test_dir)
+            with tempfile.TemporaryDirectory() as tempdir:
+                multiprocess_move_and_filter_reads(self.tmp_directory, tempdir, self.bam, self.readdb, trim=False,
+                                                   quality_threshold=False, worker_count=1, debug=False)
+                self.assertEqual(3, len(os.listdir(os.path.join(tempdir, "test"))))
+                shutil.rmtree(self.test_dir)
+                shutil.copytree(self.dna_dir, self.test_dir)
 
     def test_multiprocess_filter_reads(self):
-        reads = multiprocess_filter_reads(self.tmp_directory, self.bam, self.readdb, trim=False,
-                                          quality_threshold=False, worker_count=1, debug=True)
-        self.assertEqual(len(reads), len(list_dir(self.test_dir, ext="fast5")))
-        counter = 0
-        for read, sam_str in multiprocess_filter_reads(self.tmp_directory, self.bam, self.readdb, trim=False,
-                                                       quality_threshold=False, worker_count=1, debug=False):
-            self.assertTrue(os.path.exists(read))
-            counter += 1
-        self.assertEqual(counter, len(list_dir(self.test_dir, ext="fast5")))
+        with captured_output() as (_, _):
+
+            reads = multiprocess_filter_reads(self.tmp_directory, self.bam, self.readdb, trim=False,
+                                              quality_threshold=False, worker_count=1, debug=True)
+            self.assertEqual(len(reads), len(list_dir(self.test_dir, ext="fast5")))
+            counter = 0
+            for read, sam_str in multiprocess_filter_reads(self.tmp_directory, self.bam, self.readdb, trim=False,
+                                                           quality_threshold=False, worker_count=1, debug=False):
+                self.assertTrue(os.path.exists(read))
+                counter += 1
+            self.assertEqual(counter, len(list_dir(self.test_dir, ext="fast5")))
 
     def test_find_fast5s_from_ids_readdb(self):
         read_ids = ["5cc86bac-79fd-4897-8631-8f1c55954a45"]
